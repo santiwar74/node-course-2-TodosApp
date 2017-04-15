0fa137d20a1dfa25fe9d5f99aa5c2497b4ddbcc5
snjytwri <sanjay.tiwari@tribridge.com>
2017-04-15 10:11:24 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
  
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 
  var {mongoose} = require('./db/mongoose.js');
@@ -34,6 +35,23 @@ app.get('/todos',(req,res)=>{
     },(e)=>{
  res.status(400).send(e);
     });
+
+});
+
+app.get('/todos/:id',(req,res)=>{
+
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+    return res.status(400).send();
+    }
+    ToDo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send({});
+        };
+        res.send({
+            todo
+        });
+    }).catch((e)=>{ res.status(400).send({});})
 
 });
 

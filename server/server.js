2f914bab3b512dcafb7ecd3cb5186250dee5988e
snjytwri <sanjay.tiwari@tribridge.com>
2017-04-15 15:49:01 -0500

    Add PATCH to /todos/id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
  
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 
@@ -69,6 +70,34 @@ app.delete('/todos/:id',(req,res)=>{
         res.send({todo});
     }).catch((e)=>{ res.status(400).send();
     });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+ var id = req.params.id;
+ var body = _.pick(req.body,['text','completed']);
+
+if(!ObjectID.isValid(id)){
+    return res.status(400).send();
+}
+
+if(_.isBoolean(body.completed) && body.completed)
+{
+    body.completedAt = new Date().getTime();
+}
+else{
+    body.completed = false;
+    body.completedAt = null;
+}
+
+ToDo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo){
+        return res.status(404).send();
+    }
+    res.send({todo});
+}).catch((e)=>{
+    res.status(400).send(e);
+});
+
 });
 
 
